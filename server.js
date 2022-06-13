fcd8f691bf849198976be63bce6694d5edccd46e
milad.xsar23 <milad.xsar72@gmail.com>
2022-06-13 17:30:00 +0430

    start refactoring project for add projects"

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
-server.use(middlewares)
-server.use(router)
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    next()
-})
-server.listen(3002, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+server.options("*", cors());
+server.listen(3001, () => {
+  console.log("JSON Server is running");
+});
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_BASE_URL = "http://localhost:3002";
+const API_BASE_URL = "http://localhost:3001";
+axios.defaults.withCredentials = true
 const client = axios.create({
   baseURL: API_BASE_URL,
   headers: {
