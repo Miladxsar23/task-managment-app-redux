05578a8c39e875b8c55b3aa939f9fcb638d0cba6
milad.xsar23 <milad.xsar72@gmail.com>
2022-07-09 18:32:43 +0430

    add selector test that created By reslector

--- src/__tests__/selectors/projects.test.js
+++ src/__tests__/selectors/projects.test.js
@@ -4,7 +4,7 @@ import {
   getFilteredTasks,
   getGroupAndFilteredTasks,
 } from "../../reducers/projects";
-import { cloneDeep } from "lodash";
+import { cloneDeep, get } from "lodash";
 
 describe("project selectors", () => {
   const state = {
@@ -20,10 +20,10 @@ describe("project selectors", () => {
       isLoading: false,
       error: null,
       items: {
-        0: { id: 0, title: "test Selector", projectId: 0 },
-        1: { id: 1, title: "redux react", projectId: 0 },
-        2: { id: 2, title: "reselectors", projectId: 1 },
-        3: { id: 3, title: "memoizing", projectId: 1 },
+        0: { id: 0, title: "test Selector", status: "Unstarted", projectId: 0 },
+        1: { id: 1, title: "redux react",status: "In Progress", projectId: 0 },
+        2: { id: 2, title: "reselectors",status:"Completed", projectId: 1 },
+        3: { id: 3, title: "memoizing",status: "Unstarted", projectId: 1 },
       },
     },
     page: {
@@ -45,9 +45,36 @@ describe("project selectors", () => {
   });
   test("should retrieve tasks from getTaskByProjectId selector", () => {
     const expectedValue = [
-      { id: 0, title: "test Selector", projectId: 0 },
-      { id: 1, title: "redux react", projectId: 0 },
+      { id: 0, title: "test Selector", status: "Unstarted", projectId: 0 },
+      { id: 1, title: "redux react",status: "In Progress", projectId: 0 },
     ];
     expect(getTaskByProjectId(state)).toEqual(expectedValue);
   });
+  test("should return tasks from getFilteredTasks selector", () => {
+    const expectedTasks = [
+      { id: 1, title: "redux react",status: "In Progress", projectId: 0 },
+    ];
+    expect(getFilteredTasks(state)).toEqual(expectedTasks);
+  });
+  test("should minimally recompute the state when getFilteredTasks is called", () => {
+    const similarSearch = cloneDeep(state)
+    similarSearch.page.taskSearchTerm = "re"
+    const uniqueSearch = cloneDeep(state)
+    uniqueSearch.page.taskSearchTerm = "sele"
+    expect(getFilteredTasks.recomputations()).toEqual(0)
+    getFilteredTasks(state)
+    getFilteredTasks(similarSearch)
+    expect(getFilteredTasks.recomputations()).toEqual(1)
+    getFilteredTasks(uniqueSearch)
+    expect(getFilteredTasks.recomputations()).toEqual(2)
+  })
+  test("should return grouped tasks from getGroupAndFilteredTasks selector", () => {
+   const expectedValue = {
+    "Unstarted" : [], 
+    "In Progress" : [{ id: 1, title: "redux react",status: "In Progress", projectId: 0 }], 
+    "Completed" : []
+   }
+   console.log(getGroupAndFilteredTasks(state))
+   expect(getGroupAndFilteredTasks(state)).toEqual(expectedValue)
+  })
 });
