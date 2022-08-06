6fee37a31048731a96d7fd351cac71400ee74f63
milad.xsar23 <milad.xsar72@gmail.com>
2022-08-06 19:09:25 +0430

    add mockstore for test ontainer component

--- src/__tests__/components/container/App.test.js
+++ src/__tests__/components/container/App.test.js
@@ -1,4 +1,7 @@
-import { App } from "../../../App";
+import ConnectedApp, { App } from "../../../App";
+import configureMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import  thunk  from "redux-thunk";
 import { render, screen } from "@testing-library/react";
 
 describe("App", () => {
@@ -10,5 +13,32 @@ describe("App", () => {
     const spy = jest.fn();
     render(<App error="Ooops!" projects={[]} dispatch={spy} />);
     expect(spy).toHaveBeenCalled();
+  });
+  test("should fetch project on mount", () => {
+    const middlewares = [thunk]
+    const store = configureMockStore(middlewares)({
+      projects: {
+        items: {},
+        isLoading: false,
+        error: null,
+      },
+      tasks: {
+        items: {},
+        isLoading: false,
+        error: null,
+      },
+      page: {},
+    });
+    console.log(store)
+    render(
+      <Provider store={store}>
+        <ConnectedApp />
+      </Provider>
+    );
+    const expectedAction = {
+      type: "FETCH_PROJECTS_STARTED",
+      payLoad: {boards : undefined},
+    };
+    expect(store.getActions()[0]).toEqual(expectedAction);
   });
 });
